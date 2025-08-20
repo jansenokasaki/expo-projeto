074fcf0e0b6b5fbec5f7e76b46d05b3485c3239a
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-08-20 00:34:05 +0000

    Começo da atividade

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  image: {
+    width: 180,
+    height: 180
+  }
+})
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Image style={styles.image}  source = {require("../assets/images/Jansen.jpeg")} />
+      <Text>Jansen Okasaki</Text>
+      <Text>Sou Jansen Okasaki, estudante do 4º período de Sistemas para Internet na UNICAP. Tenho interesse em desenvolvimento web e estou sempre buscando aprender novas tecnologias e me envolver em projetos que ampliem meus conhecimentos na área.</Text>
+    </View>
+  );
+}
