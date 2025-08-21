bd9096ae124ada923ea0f373b6e7c98beb9fe58d
Jansen Okasaki <jansencruzyuki@gmail.com>
2025-08-21 23:18:31 +0000

    Ajustes finais

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,10 @@ const styles = StyleSheet.create({
   image: {
     width: 180,
     height: 180
+  }, 
+
+  bioText: {
+    textAlign: "center"
   }
 })
 export default function Index() {
@@ -13,11 +17,12 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 25
       }}
     >
       <Image style={styles.image}  source = {require("../assets/images/Jansen.jpeg")} />
       <Text>Jansen Okasaki</Text>
-      <Text>Sou Jansen Okasaki, estudante do 4º período de Sistemas para Internet na UNICAP. Tenho interesse em desenvolvimento web e estou sempre buscando aprender novas tecnologias e me envolver em projetos que ampliem meus conhecimentos na área.</Text>
+      <Text style={styles.bioText}>Sou Jansen Okasaki, estudante do 4º período de Sistemas para Internet na UNICAP. Tenho interesse em desenvolvimento web e estou sempre buscando aprender novas tecnologias e me envolver em projetos que ampliem meus conhecimentos na área.</Text>
     </View>
   );
 }
